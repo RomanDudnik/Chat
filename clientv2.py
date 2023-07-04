38eb274b9c54e5b1ec293041d8c3e17c62650912
«Roman <research777@mail.ru>
2023-07-04 16:19:43 +0300

    RenaME

--- clientv2.py
+++ clientv2.py
@@ -8,27 +8,27 @@ class ChatClient:
         self.port = port
         self.server_socket = None
 
-def start(self):
-    self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    self.client_socket.connect((self.ip, self.port))
-    self.client_socket.sendall(self.nickname.encode("utf-8"))
+    def start(self):
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect((self.ip, self.port))
+        self.client_socket.sendall(self.nickname.encode("utf-8"))
     
-    receive_thread = threading.Thread(target = self.receive_messages)
-    receive_thread.start()
+        receive_thread = threading.Thread(target = self.receive_messages)
+        receive_thread.start()
     
-    while True:
-        message = input()
-        self.client_socket.sendall(message.encode("utf-8"))
+        while True:
+            message = input()
+            self.client_socket.sendall(message.encode("utf-8"))
         
-def receive_messages(self):
-    while True:
-        try:
-            message = self.client_socket.recv(1024).decode("utf-8")
-            print(message)
+    def receive_messages(self):
+        while True:
+            try:
+                message = self.client_socket.recv(1024).decode("utf-8")
+                print(message)
         
-        except Exception as e:
-            print(f"Error occurred: {e}")
-            break
+            except Exception as e:
+                print(f"Error occurred: {e}")
+                break
         
 if __name__ == "__main__":
     nickname = input("Enter your nickname: ")
--- serverv2.py
+++ serverv2.py
@@ -8,48 +8,48 @@ class ChatServer:
         self.server_socket = None
         self.clients = []
         
-def start(self):
-    self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    self.server_socket.bind((self.ip, self.port))
-    self.server_socket.listen(5)
+    def start(self):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind((self.ip, self.port))
+        self.server_socket.listen(5)
     
-    print(f"Chat server started on {self.ip}:{self.port}")
+        print(f"Chat server started on {self.ip}:{self.port}")
     
-    while True:
-        client_socket, addr = self.server_socket.accept()
-        client_thread = threading.Thread(target = self.handle_client, args = (client_socket,))
-        client_thread.start()
+        while True:
+            client_socket, addr = self.server_socket.accept()
+            client_thread = threading.Thread(target = self.handle_client, args = (client_socket,))
+            client_thread.start()
 
-def handle_client(self, client_socket):
-    client_name = client_socket.recv(1024).decode("utf-8")
-    self.broadcast_messege(f"{client_name} joined the chat!")
-    self.clients.append((client_name, client_socket))
+    def handle_client(self, client_socket):
+        client_name = client_socket.recv(1024).decode("utf-8")
+        self.broadcast_message(f"{client_name} joined the chat!")
+        self.clients.append((client_name, client_socket))
     
-    while True:
-        try:
-            messege = client_socket.recv(1024).decode("utf-8")
+        while True:
+            try:
+                messege = client_socket.recv(1024).decode("utf-8")
             
-            if messege:
-                self.broadcast_messege(f"{client_name}: {messege}")
-            else:
+                if messege:
+                    self.broadcast_messege(f"{client_name}: {messege}")
+                else:
+                    self.remove_client(client_name, client_socket)
+                    break
+            except Exception as e:
+                print(f"Error occurred: {e}")
                 self.remove_client(client_name, client_socket)
                 break
-        except Exception as e:
-            print(f"Error occurred: {e}")
-            self.remove_client(client_name, client_socket)
-            break
 
-def remove_client(self, client_name, client_socket):
-    self.clients.remove((client_name, client_socket))
-    self.broadcast_message(f"{client_name} left the chat.")
-    client_socket.close()
+    def remove_client(self, client_name, client_socket):
+        self.clients.remove((client_name, client_socket))
+        self.broadcast_message(f"{client_name} left the chat.")
+        client_socket.close()
     
-def broadcast_message(self, message):
-    for _, client_socket in self.clients:
-        try:
-            client_socket.sendall(message.encode("utf-8"))
-        except Exception as e:
-            print(f"Error occurred: {e}")
+    def broadcast_message(self, message):
+        for _, client_socket in self.clients:
+            try:
+                client_socket.sendall(message.encode("utf-8"))
+            except Exception as e:
+                print(f"Error occurred: {e}")
             
 if __name__ == "__main__":
     ip = input("Enter server IP:")
